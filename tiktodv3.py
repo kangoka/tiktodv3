55e0b681e29e5af4bac938a984e115b7b98af6e9
Kang Oka <51883097+kangoka@users.noreply.github.com>
2020-09-08 03:51:15 +0700

    Some update
    Update XPath for views and add loop4() for auto followers

--- tiktodv3.py
+++ tiktodv3.py
@@ -18,18 +18,18 @@ def loop1():
     global i
     time.sleep(10)
     try:
-        driver.find_element_by_xpath("/html/body/div[3]/div[1]/div[3]/div/div[4]/div/button").click()
+        driver.find_element_by_xpath("/html/body/div[4]/div[1]/div[3]/div/div[4]/div/button").click()
     except:
         print("You didn't solve the captcha yet. Need to refresh to avoid endless loop.")
         driver.refresh()
         loop1()
     try:
         time.sleep(2)
-        driver.find_element_by_xpath("/html/body/div[3]/div[4]/div/div/div/form/div/input").send_keys(vidUrl)
+        driver.find_element_by_xpath("/html/body/div[4]/div[4]/div/div/div/form/div/input").send_keys(vidUrl)
         time.sleep(1)
-        driver.find_element_by_xpath("/html/body/div[3]/div[4]/div/div/div/form/div/div/button").click()
+        driver.find_element_by_xpath("/html/body/div[4]/div[4]/div/div/div/form/div/div/button").click()
         time.sleep(2)
-        driver.find_element_by_xpath("/html/body/div[3]/div[4]/div/div/div/div/div/div[1]/div/form/button").click()
+        driver.find_element_by_xpath("/html/body/div[4]/div[4]/div/div/div/div/div[1]/div/form/button").click()
         driver.refresh()
         i += 1
         total = i * 1000
@@ -67,7 +67,7 @@ def loop2():
         print("An error occured. Now will retry again")
         driver.refresh()
         loop2()
-"""
+
 def loop3():
     def hearts():
         global i, x, a
@@ -84,8 +84,9 @@ def loop3():
             driver.find_element_by_xpath("/html/body/div[3]/div[3]/div/div/div/form/div/input").send_keys(vidUrl)
             time.sleep(1)
             driver.find_element_by_xpath("/html/body/div[3]/div[3]/div/div/div/form/div/div/button").click()
-            time.sleep(2)
+            time.sleep(3)
             driver.find_element_by_xpath("/html/body/div[3]/div[3]/div/div/div/div/div[1]/div/form/button").click()
+            time.sleep(3)
             driver.refresh()
             a += 1
             total = a * 25
@@ -128,9 +129,35 @@ def loop3():
             views()
 
     hearts()
-"""
+
+def loop4():
+    time.sleep(10)
+    try:
+        driver.find_element_by_xpath("/html/body/div[4]/div[1]/div[3]/div/div[1]/div/button").click()
+    except:
+        print("You didn't solve the captcha yet. Need to refresh to avoid endless loop.")
+        driver.refresh()
+        loop4()
+    try:
+        time.sleep(2)
+        driver.find_element_by_xpath("/html/body/div[4]/div[2]/div/div/div/form/div/input").send_keys(username)
+        time.sleep(1)
+        driver.find_element_by_xpath("/html/body/div[4]/div[2]/div/div/div/form/div/div/button").click()
+        time.sleep(3)
+        driver.find_element_by_xpath("/html/body/div[4]/div[2]/div/div/div/div/form/button").click()
+        time.sleep(2)
+        folls = driver.find_element_by_xpath('//*[@id="c2VuZF9mb2xsb3dlcnNfdGlrdG9r"]/span').text
+        print(folls)
+        driver.refresh()
+        time.sleep(318)
+        loop4()
+    except:
+        print("An error occured. Now will retry again")
+        driver.refresh()
+        loop4()
 
 vidUrl = "YOUR_URL" #Change YOUR_URL to your Tik Tok video URL. This URL used to get views or hearts or both
+username = "YOUR_USERNAME" #Change YOUR_USERNAME to your Tik Tok username
 
 system("cls")
 tiktod = pyfiglet.figlet_format("TIKTOD V3", font="slant")
@@ -142,13 +169,20 @@ print("")
 You can change auto value below
 auto = 1 for auto views
 auto = 2 for auto hearts
-auto = 3 for auto views + hearts (NEW UPDATE: CAN'T RUN AUTO VIEWS + AUTO HEARTS AT THE SAME TIME)
+auto = 3 for auto views + hearts (PATCHED)
+auto = 4 for auto followers
 """
-auto = 1
+auto = 4 #Change this
 
 if auto == 1:
     driver.get("https://vipto.de/")
     loop1()
-else:
+elif auto == 2:
     driver.get("https://vipto.de/")
     loop2()
+elif auto == 3:
+    driver.get("https://vipto.de/")
+    loop3()
+else:
+    driver.get("https://vipto.de/")
+    loop4()
