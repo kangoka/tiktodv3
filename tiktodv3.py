ec05a568eaa4e2098fe9142fa394ef63fc76d9cd
Kang Oka <51883097+kangoka@users.noreply.github.com>
2020-11-27 08:59:23 +0700

    Major Update

--- tiktodv3.py
+++ tiktodv3.py
@@ -1,189 +1,153 @@
 from selenium import webdriver
-from selenium.webdriver.support.ui import Select
-import pyfiglet
 from os import system
-import time
+from time import time, strftime, gmtime, sleep
+import pyfiglet, os, threading
+
+system("cls")
+os.system('title TIKTOD V3')
+print(pyfiglet.figlet_format("TIKTOD V3", font="slant"))
+print("Author: https://github.com/kangoka\n")
+print("1. Auto Views\n2. Auto Hearts\n3. Auto Followers\n")
+auto = int(input("Select auto: "))
+vidUrl = input("TikTok video URL: ")
+
+start = time()
+time_elapsed = strftime('%H:%M:%S', gmtime(time() - start))
 
 chrome_options = webdriver.ChromeOptions()
-#chrome_options.add_argument("--headless")
-chrome_options.add_argument("--log-level=3")
+chrome_options.add_argument("--mute-audio")
+chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
 driver = webdriver.Chrome(r"chromedriver.exe", options=chrome_options)
 driver.set_window_size(1024, 650)
 
-i = 0
-a = 0
-x = 0
+Views = 0
+Hearts = 0
+Followers = 0
+
+def beautify(arg):
+    return format(arg, ',d').replace(',', '.')
+
+def title1():
+    global Views
+    while True:
+        time_elapsed = strftime('%H:%M:%S', gmtime(time() - start))
+        os.system(f'title TIKTOD V3 ^| Views Sent: {beautify(Views)} ^| Elapsed Time: {time_elapsed}')
+
+def title2():
+    global Hearts
+    while True:
+        time_elapsed = strftime('%H:%M:%S', gmtime(time() - start))
+        os.system(f'title TIKTOD V3 ^| Hearts Sent: {beautify(Hearts)} ^| Elapsed Time: {time_elapsed}')
+
+def title3():
+    global Followers
+    while True:
+        time_elapsed = strftime('%H:%M:%S', gmtime(time() - start))
+        os.system(f'title TIKTOD V3 ^| Followers Sent: {beautify(Followers)} ^| Elapsed Time: {time_elapsed}')
+    
 
 def loop1():
-    global i
-    time.sleep(10)
+    global Views
+    sleep(10)
     try:
         driver.find_element_by_xpath("/html/body/div[4]/div[1]/div[3]/div/div[4]/div/button").click()
     except:
-        print("You didn't solve the captcha yet. Need to refresh to avoid endless loop.")
+        print("> Solve the captcha!")
         driver.refresh()
         loop1()
     try:
-        time.sleep(2)
+        sleep(2)
         driver.find_element_by_xpath("//*[@id=\"sid4\"]/div/div/div/form/div/input").send_keys(vidUrl)
-        time.sleep(1)
+        sleep(1)
         driver.find_element_by_xpath("//*[@id=\"sid4\"]/div/div/div/form/div/div/button").click()
-        time.sleep(2)
+        sleep(5)
         driver.find_element_by_xpath("//*[@id=\"c2VuZC9mb2xsb3dlcnNfdGlrdG9V\"]/div[1]/div/form/button").click()
-        time.sleep(3)
         driver.refresh()
-        i += 1
-        total = i * 1000
-        print("Views success delivered! Total", total,"views")
-        time.sleep(60)
+        Views += 1000
+        sleep(60)
         loop1()
     except:
-        print("An error occured. Now will retry again")
+        print("> An error occured. Now will retry again")
         driver.refresh()
         loop1()
 
 def loop2():
-    global i
-    time.sleep(10)
+    global Hearts
+    sleep(10)
     try:
         driver.find_element_by_xpath("/html/body/div[4]/div[1]/div[3]/div/div[2]/div/button").click()
     except:
-        print("You didn't solve the captcha yet. Need to refresh to avoid endless loop.")
+        print("> Solve the captcha!")
         driver.refresh()
         loop2()
     try:
-        time.sleep(2)
+        sleep(2)
         driver.find_element_by_xpath('//*[@id="sid2"]/div/div/div/form/div/input').send_keys(vidUrl)
-        time.sleep(1)
+        sleep(1)
         driver.find_element_by_xpath('//*[@id="sid2"]/div/div/div/form/div/div/button').click()
-        time.sleep(5)
+        sleep(5)
         driver.find_element_by_xpath('//*[@id="c2VuZE9nb2xsb3dlcnNfdGlrdG9r"]/div[1]/div/form/button').click()
-        time.sleep(6)
-        hearts = driver.find_element_by_xpath('//*[@id="c2VuZE9nb2xsb3dlcnNfdGlrdG9r"]/span').text
-        time.sleep(1)
+        sleep(6)
+        hearts = driver.find_element_by_xpath('//*[@id="c2VuZE9nb2xsb3dlcnNfdGlrdG9r"]/span').text.split()
+        Hearts += int(hearts[0])
+        sleep(5)
         driver.refresh()
-        print(hearts)
-        time.sleep(310)
+        sleep(640)
         loop2()
     except:
-        print("An error occured. Now will retry again")
+        print("> An error occured. Now will retry again")
         driver.refresh()
         loop2()
 
 def loop3():
-    def hearts():
-        global i, x, a
-        x = 0
-        time.sleep(10)
-        try:
-            driver.find_element_by_xpath("/html/body/div[3]/div[1]/div[3]/div/div[2]/div/button").click()
-        except:
-            print("You didn't solve the captcha yet. Need to refresh to avoid endless loop.")
-            driver.refresh()
-            hearts()
-        try:
-            time.sleep(2)
-            driver.find_element_by_xpath("/html/body/div[3]/div[3]/div/div/div/form/div/input").send_keys(vidUrl)
-            time.sleep(1)
-            driver.find_element_by_xpath("/html/body/div[3]/div[3]/div/div/div/form/div/div/button").click()
-            time.sleep(3)
-            driver.find_element_by_xpath("/html/body/div[3]/div[3]/div/div/div/div/div[1]/div/form/button").click()
-            time.sleep(3)
-            driver.refresh()
-            a += 1
-            total = a * 25
-            print("Hearts success delivered! Total estimated", total,"hearts")
-            views()
-        except:
-            print("An error occured. Now will retry again")
-            driver.refresh()
-            hearts()
-
-    def views():
-        global i, x
-        time.sleep(10)
-        try:
-            driver.find_element_by_xpath("/html/body/div[3]/div[1]/div[3]/div/div[4]/div/button").click()
-        except:
-            print("You didn't solve the captcha yet. Need to refresh to avoid endless loop.")
-            driver.refresh()
-            views()
-        try:
-            time.sleep(2)
-            driver.find_element_by_xpath("/html/body/div[3]/div[4]/div/div/div/form/div/input").send_keys(vidUrl)
-            time.sleep(1)
-            driver.find_element_by_xpath("/html/body/div[3]/div[4]/div/div/div/form/div/div/button").click()
-            time.sleep(2)
-            driver.find_element_by_xpath("/html/body/div[3]/div[4]/div/div/div/div/div/div[1]/div/form/button").click()
-            driver.refresh()
-            x += 1
-            i += 1
-            total = i * 1000
-            print("Views success delivered! Total", total,"views")
-            if x < 5:
-                time.sleep(50)
-                views()
-            else:
-                hearts()
-        except:
-            print("An error occured. Now will retry again")
-            driver.refresh()
-            views()
-
-    hearts()
-
-def loop4():
-    time.sleep(10)
+    global Followers
+    sleep(10)
     try:
         driver.find_element_by_xpath("/html/body/div[4]/div[1]/div[3]/div/div[1]/div/button").click()
     except:
-        print("You didn't solve the captcha yet. Need to refresh to avoid endless loop.")
+        print("> Solve the captcha!")
         driver.refresh()
-        loop4()
+        loop3()
     try:
-        time.sleep(2)
+        sleep(2)
         driver.find_element_by_xpath('//*[@id="sid"]/div/div/div/form/div/input').send_keys(vidUrl)
-        time.sleep(1)
+        sleep(1)
         driver.find_element_by_xpath('//*[@id="sid"]/div/div/div/form/div/div/button').click()
-        time.sleep(5)
-        driver.find_element_by_xpath('//*[@id="c2VuZF9mb2xsb3dlcnNfdGlrdG9r"]/div[1]/div/form/button').click()
-        time.sleep(6)
-        folls = driver.find_element_by_xpath('//*[@id="c2VuZF9mb2xsb3dlcnNfdGlrdG9r"]/span').text
-        print(folls)
+        sleep(5)
+        driver.find_element_by_xpath().click('//*[@id="c2VuZF9mb2xsb3dlcnNfdGlrdG9r"]/div[1]/div/form/button')
+        sleep(6)
+        folls = driver.find_element_by_xpath('//*[@id="c2VuZF9mb2xsb3dlcnNfdGlrdG9r"]/span').text.split()
+        Followers += int(folls[0])
         driver.refresh()
-        time.sleep(310)
-        loop4()
+        sleep(310)
+        loop3()
     except:
-        print("An error occured. Now will retry again")
+        print("> An error occured. Now will retry again")
         driver.refresh()
-        loop4()
-
-vidUrl = "YOUR_URL" #Change YOUR_URL to your Tik Tok video URL.
+        loop3()
 
 system("cls")
-tiktod = pyfiglet.figlet_format("TIKTOD V3", font="slant")
-print(tiktod)
-print("Author: https://github.com/kangoka")
-print("")
-
-"""
-You can change auto value below
-auto = 1 for auto views
-auto = 2 for auto hearts
-auto = 3 for auto views + hearts (PATCHED)
-auto = 4 for auto followers
-"""
-auto = 1 #Change this
+print(pyfiglet.figlet_format("TIKTOD V3", font="slant"))
+print("Author: https://github.com/kangoka\n")
+print("Error Log:")
 
 if auto == 1:
     driver.get("https://ketuy.com/")
-    loop1()
+    a = threading.Thread(target=title1)
+    b = threading.Thread(target=loop1)
+    a.start()
+    b.start()
 elif auto == 2:
     driver.get("https://ketuy.com/")
-    loop2()
+    a = threading.Thread(target=title2)
+    b = threading.Thread(target=loop2)
+    a.start()
+    b.start()
 elif auto == 3:
     driver.get("https://ketuy.com/")
-    loop3()
+    a = threading.Thread(target=title3)
+    b = threading.Thread(target=loop3)
+    a.start()
+    b.start()
 else:
-    driver.get("https://ketuy.com/")
-    loop4()
+    print("Input between 1-3")
